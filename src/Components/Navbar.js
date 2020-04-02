48ca0051f1cf9d9747cf9d8304e5fce940cdf1ad
ehayne00 <ehayne00@hotmail.com>
2020-04-02 23:08:28 +0100

    update

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+    return(
+        <div>
+            <h1>Joe Grant Fitness</h1>
+            <h2>Mobile Personal</h2>
+        </div>
+    )
+}
+
+export default NavBar;
